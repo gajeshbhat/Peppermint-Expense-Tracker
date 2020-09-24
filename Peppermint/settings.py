64edb167cb5293d2cb46d4c3db6ddc93e888308f
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2020-09-24 11:15:55 -0400

    Add .env file and updated DB env variables

--- Peppermint/settings.py
+++ Peppermint/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'Peppermint.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'peppermint_store',
-        'USER': 'django_pepper_plug',
-        'PASSWORD': 'peppermint_app',
-        'HOST': 'localhost',
-        'PORT': "5432",  # Default PostgresDB PORT
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),  # Default PostgresDB PORT
     }
 }
 
